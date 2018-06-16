4e2f088de57449de7a6f1e32346ae19735eb1104
Sebastian Amenabar <samenabar@uc.cl>
2018-06-16 13:12:21 -0400

    2.1: Fix runner method

--- 2.1 - Remove dups.py
+++ 2.1 - Remove dups.py
@@ -1,4 +1,5 @@
-from _utils import LinkedList
+from copy import deepcopy
+from _utils import DoubleLinkedList
 
 # Runtime: O(n)
 # Memory: O(n)
@@ -17,8 +18,8 @@ def remove_dups_hash(head):
 # Memory: O(1)
 def remove_dups_runner(head):
     node = head
-    runner = head.next
     while node:
+        runner = node.next
         while runner:
             if node.value == runner.value:
                 runner.remove()
@@ -27,10 +28,10 @@ def remove_dups_runner(head):
     return head
 
 if __name__ == '__main__':
-    head = LinkedList(0)
+    head = DoubleLinkedList(0)
     node = head
     for value in [1, 3, 3, 3, 0, 5, 1, 2, 3, 4, 0, 2, 2, 2, 2, 2]:
-        node.next = LinkedList(value, node)
+        node.next = DoubleLinkedList(value, node)
         node = node.next
-    print(remove_dups_hash(head))
-    print(remove_dups_runner(head))
+    print(remove_dups_hash(deepcopy(head)))
+    print(remove_dups_runner(deepcopy(head)))
