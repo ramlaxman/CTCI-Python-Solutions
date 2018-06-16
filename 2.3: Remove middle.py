12ee95f3c14e0e4a1281f571df98afa828f4aeb4
Sebastian Amenabar <samenabar@uc.cl>
2018-06-16 14:40:46 -0400

    2.3: Remove middle

---
+++ 2.3: Remove middle.py
@@ -0,0 +1,28 @@
+from _utils import SingleLinkedList
+
+def remove_middle(node):
+    # Is not tail
+    if node.next:
+        node.value = node.next.value
+        if node.next.next:
+            node.next = node.next.next
+        else:
+            node.next = None
+
+if __name__ == '__main__':
+    head = SingleLinkedList(0)
+    node = head
+    for i in range(1, 5):
+        node.next = SingleLinkedList(i)
+        node = node.next
+    node = head
+    for i in range(3):
+        node = node.next
+    print(head)
+    remove_middle(node)
+    print(head)
+    # Does not delete tail
+    remove_middle(node)
+    print(head)
+    remove_middle(head.next)
+    print(head)
