849e811bd807cbb698adc6d2baa92850039b4be4
Sebastian Amenabar <samenabar@uc.cl>
2018-06-15 23:29:09 -0400

    1.8: Reduce memory consumption to O(1)

--- 1.8 - Zero Matrix.py
+++ 1.8 - Zero Matrix.py
@@ -1,25 +1,50 @@
 import _matrices as matrices
 from _utils import print_matrix
 
-# Runtime: O(N * M)
-# Memory: O(N + M)
-def zero_matrix(matrix):
-    rows_to_clear = [False for _ in range(len(matrix))]
-    columns_to_clear = [False for _ in range(len(matrix[0]))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] == 0:
-                rows_to_clear[i] = True
-                columns_to_clear[j] = True
 
-    for row, clear in enumerate(rows_to_clear):
-        if clear:
-            for column in range(len(matrix[0])):
-                matrix[row][column] = 0
-    for column, clear in enumerate(columns_to_clear):
-        if clear:
-            for row in range(len(matrix)):
-                matrix[row][column] = 0
+def zero_row(row, matrix):
+    for column in range(len(matrix[0])):
+        matrix[row][column] = 0
+
+def zero_column(column, matrix):
+    for row in range(len(matrix)):
+        matrix[row][column] = 0
+
+# Runtime: O(N * M)
+# Memory: O(1)
+def zero_matrix(matrix):
+
+    first_column_has_zero = False
+    first_row_has_zero = False
+    for row in range(len(matrix)):
+        if matrix[row][0] == 0:
+            first_column_has_zero = True
+    for column in range(len(matrix[0])):
+        if matrix[0][column] == 0:
+            first_row_has_zero = True
+
+    # Leave the first row/column for the end
+    # so it can be used to store the columns/rows to zero
+    for i in range(1, len(matrix)):
+        for j in range(1, len(matrix[0])):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+
+    # Empty the rows
+    for row in range(1, len(matrix)):
+        if matrix[row][0] == 0:
+            zero_row(row, matrix)
+
+    # Empty the columns
+    for column in range(1, len(matrix[0])):
+        if matrix[0][column] == 0:
+            zero_column(column, matrix)
+
+    if first_column_has_zero:
+        zero_column(0, matrix)
+    if first_row_has_zero:
+        zero_row(0, matrix)
 
     return matrix
 
@@ -30,4 +55,5 @@ if __name__ == '__main__':
 
     matrices.unsquare_matrix[2][2] = 0
     matrices.unsquare_matrix[0][2] = 0
+    matrices.unsquare_matrix[0][0] = 0
     print_matrix(zero_matrix(matrices.unsquare_matrix))
