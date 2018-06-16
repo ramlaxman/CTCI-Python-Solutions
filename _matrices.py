ad21a8cf14e4cbf7a7b79086c3a23e3f17d8c5f9
Sebastian Amenabar <samenabar@uc.cl>
2018-06-15 23:14:01 -0400

    Util files

---
+++ _matrices.py
@@ -0,0 +1,16 @@
+invalid_matrix = []
+
+one_by_one_matrix = [[1]]
+
+square_uneven_matrix =  [[1, 2, 3],
+                         [4, 5, 6],
+                         [7, 8, 9]]
+
+square_even_matrix =    [[ 1,  2,  3,  4],
+                         [ 5,  6,  7,  8],
+                         [ 9, 10, 11, 12],
+                         [13, 14, 15, 16]]
+
+unsquare_matrix =       [[1,  2,  3,  4],
+                         [5,  6,  7,  8],
+                         [9, 10, 11, 12]]
---
+++ _utils.py
@@ -0,0 +1,2 @@
+def print_matrix(matrix):
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
